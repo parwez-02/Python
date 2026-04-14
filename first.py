49be395d4efbace1fe5c802c48f04ea8addfafd2
parwez-02 <parwezhosen02@gmail.com>
2026-04-14 15:29:06 +0530

    add files

---
+++ first.py
@@ -0,0 +1,34 @@
+# print("Hello World")
+
+"""
+So
+That was my program
+"""
+'''import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)'''
+
+""" print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the trav'ller in the dark,
+Thanks you for your tiny spark,
+He could not see which way to go,
+If you did not twinkle so.''') """
+
+
+# import pyttsx3
+# engine = pyttsx3.init()
+
+# # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
+
+# engine.say("I will speak this text")
+# engine.runAndWait()
